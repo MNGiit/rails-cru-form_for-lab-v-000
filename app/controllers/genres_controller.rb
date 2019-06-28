8edb19cbca14063328232c6f3f89d6fe7ad1f725
Mohammed Noori <kmanoorib@gmail.com>
2019-06-28 02:38:41 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,22 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+  
+  def new
+    @genre = Genre.new
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    
+    redirect_to genre_path
+  end
+  
+  private
+  
+  def genre_params
+    
+  end
+  
 end
