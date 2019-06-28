757a6bc0c1c820c9ec7c537d4a89a2f1d72a0565
Mohammed Noori <kmanoorib@gmail.com>
2019-06-28 02:29:27 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,11 +9,17 @@ class ArtistsController < ApplicationController
   
   def create
     @artist = Artist.new(artist_params)
+    @artist.save
+    
+    redirect_to artist_path(@artist)
   end
   
   private
   
   def artist_params
-    
+    params.require(:artist).permit(:name, :bio)
   end
+  #def school_class_params # (*args)
+  #  params.require(:school_class).permit(:title, :room_number)
+  #end
 end
